862a082194f7b1396e642dd3f7eab2a24e202d97
nguyenanhtu123 <lovebooks29062k2@gmail.com>
2024-01-15 10:39:36 +0700

    update controller

--- src/main/java/com/example/springproject/configuration/ServiceConfiguration.java
+++ src/main/java/com/example/springproject/configuration/ServiceConfiguration.java
@@ -1,8 +1,12 @@
 package com.example.springproject.configuration;
 
 
+import com.example.springproject.repository.UserRepository;
+import com.example.springproject.service.UserService;
 import com.example.springproject.service.base.MessageService;
 import com.example.springproject.service.base.MessageServiceImpl;
+import com.example.springproject.service.impl.JwtService;
+import com.example.springproject.service.impl.UserServiceImpl;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Bean;
@@ -12,7 +16,10 @@ import org.springframework.context.annotation.Bean;
  */
 @Configuration
 public class ServiceConfiguration {
-
+  @Bean
+  public UserService userService(UserRepository repository, JwtService jwtService) {
+    return new UserServiceImpl(repository, jwtService);
+  }
   @Bean
   public MessageService messageService(MessageSource messageSource) {
     return new MessageServiceImpl(messageSource);
--- src/main/java/com/example/springproject/constant/MessageCodeConstant.java
+++ src/main/java/com/example/springproject/constant/MessageCodeConstant.java
@@ -7,5 +7,10 @@ public class MessageCodeConstant {
     public static String REGISTER_CODE = "com.example.springproject.controller.register";
 
     public static String LOGIN_CODE = "com.example.springproject.controller.login";
+    public static final String CREATE_USER = "com.example.springproject.controller.create";
+    public static final String LIST_USER = "com.example.springproject.controller.list";
+    public static final String DELETE_USER = "com.example.springproject.controller.delete";
+    public static final String UPDATE_USER = "com.example.springproject.controller.update";
+
 
 }
